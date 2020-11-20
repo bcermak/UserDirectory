59d4e8b4177338f8c76c6eac354a796e7ac440b9
Brandon Cermak <cermakprofessional@gmail.com>
2020-11-19 20:56:24 -0600

    rebuilt app.js and added database for employee info

--- user-directory/src/App.js
+++ user-directory/src/App.js
@@ -1,10 +1,39 @@
-import './App.css';
-import FriendCard from './components/EmployeeCard/index';
+import React, { Component } from "react";
+import EmployeeCard from './components/EmployeeCard/index';
+import employees from "./employees.json";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
 
-function App() {
-  return (
-    <FriendCard></FriendCard>
-  );
+class App extends Component {
+ 
+  state = {
+    employees
+  };
+
+  removeEmployee= id => {
+    const employees = this.state.employees.filter(employee => employee.id !== id)
+    this.setState({ employees });
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Title>Employee Directory</Title>
+        
+        {this.state.employees.map(employee => (
+          <EmployeeCard
+            removeEmployee={this.removeEmployee}
+            id={employee.id}
+            key={employee.id}
+            name={employee.name}
+            jobTitle={employee.jobTitle}
+            location={employee.location}
+          />
+        ))}
+        
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
--- user-directory/src/components/EmployeeCard/index.js
+++ user-directory/src/components/EmployeeCard/index.js
@@ -1,30 +1,30 @@
 import React from "react";
 import "./style.css";
 
-function FriendCard(props) {
+function EmployeeCard(props) {
   return (
-  <body style ={{backgroundColor: "grey"}}> 
-    <div className="card">
-      <div className="img-container">
-        <img alt="userImage" src="http://images.clipartpanda.com/user-clipart-matt-icons_preferences-desktop-personal.png"/>
-      </div>
-      <div className="content">
-        <ul>
-        <li>
-            <strong>Employee Name:</strong> Brandon
-          </li>
-          <li>
-            <strong>Job Title:</strong> Programmer
-          </li>
-          <li>
-            <strong>Location:</strong> ATX
-          </li>
-        </ul>
-      </div>
-
-    </div>
-  </body>
+    <body style ={{backgroundColor: "grey"}}>
+        <div className="card">
+          <div className="img-container">
+            <img alt="userImage" src="http://images.clipartpanda.com/user-clipart-matt-icons_preferences-desktop-personal.png" />
+          </div>
+          <div className="content">
+            <ul>
+              <li>
+                <strong>Employee Name:</strong> {props.name}
+              </li>
+              <li>
+                <strong>Job Title:</strong> {props.jobTitle}
+                </li>
+              <li>
+                <strong>Location:</strong> {props.location}
+                </li>
+            </ul>
+          </div>
+        </div>
+        
+    </body>
   );
 }
 
-export default FriendCard;
+export default EmployeeCard
